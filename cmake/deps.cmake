e669e44c8805564dd8b224db00d5e15490d22c03
Michael Tsukanov <tsukanovWOW@ya.ru>
2025-11-03 20:17:28 +0300

    Hot fix

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -2,4 +2,4 @@ find_package(libassert REQUIRED)
 find_package(Boost REQUIRED)
 find_package(spdlog REQUIRED)
 
-set(MR_STEWARD_DEPENDENCIES libassert::assert spdlog::spdlog boost::boost)
+set(MR_UTILS_DEPENDENCIES libassert::assert spdlog::spdlog boost::boost)
